503168652d80217dca07db5ee192af106160bad0
minfrin <minfrin@users.noreply.github.com>
2020-08-10 16:43:39 +0200

    Update docs.

--- mod_magick_resize.c
+++ mod_magick_resize.c
@@ -22,17 +22,28 @@
  *
  * Basic configuration:
  *
- *   <Location />
- *     <IfModule magick_resize_module>
- *       <If "%{QUERY_STRING} =~ /./">
- *         SetOutputFilter MAGICK_RESIZE
- *       </If>
- *     </IfModule>
- *   </Location>
+ *   <FilesMatch ".+\.(gif|jpe?g|png)$">
+ *     <If "%{QUERY_STRING} =~ /./">
+ *       SetOutputFilter MAGICK;MAGICK_RESIZE
+ *       MagickResizeColumns %{QUERY_STRING}
+ *     </If>
+ *   </FilesMatch>
+ *
+ * In the above configuration, if we see any filenames ending in the major
+ * graphics formats, and if a query string is present containing the desired
+ * width of the image, we add the magick and magick resize filters, and
+ * request that the width be set to the value of the query string, falling
+ * back to the original width if the query string could not be interpreted as
+ * a valid number.
+ *
+ * To protect against denial of service, dimensions of the image are
+ * automatically capped at the dimensions of the original image. The browser
+ * will typically do any resizing up required.
  *
  * All resize directives take on a list of expressions, the last expression
- * to return a valid value wins. This allows support for responsive behaviour
- * such as HTTP Client Hints.
+ * to return a valid value wins. Any expression that renders an invalid value
+ * (such as the empty string) is skipped and the previous expression is tried.
+ * This allows support for responsive behaviour such as HTTP Client Hints.
  *
  * For example, here we first take into account the HTTP Client Hint "Width"
  * header, followed by the query string, followed by the default fallback value
@@ -56,21 +67,45 @@
  * In the absence of a valid fallback value, or if the fallback value is zero,
  * the original image value is maintained.
  *
- * The MagickResizeFactor can be used to apply a multiplier to the width and
- * height. For example, to use the HTTP Client Hint Save-Data header, falling
- * back to the DPR header, falling back to the default factor of 1:
+ * The MagickResizeColumns and MagickResizeRows directives control the width
+ * and height of the resulting image. If either the width or height is left
+ * unspecified it will be calculated based on the aspect ratio of the original
+ * image.
+ *
+ * The MagickResizeFilterType directive specifies the filter to use while
+ * resizing. Possible values are:
+ *
+ *   MagickResizeFilterType bessel|blackman|box|catrom|
+ *      cubic|gaussian|hamming|hanning|hermite|lanczos|mitchell|point|
+ *      quadratic|sinc|triangle
+ *
+ * The MagickResizeBlur directive specifies the amount of blur to add while
+ * resizing, where greater than 1 is blurry, and less than 1 is sharp. The
+ * default is 1.
+ *
+ * The MagickResizeFactor directive can be used to apply a multiplier to the
+ * width and height. For example, to use the HTTP Client Hint Save-Data header,
+ * falling back to the DPR header, falling back to the default factor of 1:
  *
  *   SetOutputFilter MAGICK;MAGICK_RESIZE
+ *   MagickResizeWidth 400
  *   <If "%{req:DPR} != ''">
+ *     # If the DPR is 2, the resulting width will be 800
  *     MagickResizeFactor %{req:DPR}
  *   </If>
  *   <If "%{req:Save-Data} = 'on'">
+ *     # If the Save-Data is on, the resulting width will be 200
  *     MagickResizeFactor 0.5
  *   </If>
  *
  * The MagickResizeModulus limits the possible image sizes so that caches are
  * not overwhelmed. All requested rows and columns are rounded up to the nearest
- * modulus value, maintaining the given aspect ratio.
+ * modulus value. Any calculated rows and columns value will be left unchanged
+ * to keep the aspect ratio.
+ *
+ *   # Resulting width will be 300
+ *   MagickResizeWidth 201
+ *   MagickResizeModulus 100
  */
 
 #include <apr_strings.h>
